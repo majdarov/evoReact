89689ec72be75b686211e6552e1e15824799cf61
majdarov <majdarov@gmail.com>
2021-03-27 13:12:33 +0400

    Learn TS (testTS.ts)

--- Tests/testTS.ts
+++ Tests/testTS.ts
@@ -11,7 +11,7 @@ console.log(padLeft('NEW-', 'test'));
 function firstElement<T>(arr: Array<T>): unknown {
   if (typeof arr[0] === 'number') {
     return arr[0];
-  } else if( typeof (arr[0] as unknown) === 'string') {
+  } else if (typeof (arr[0] as unknown) === 'string') {
     return ((arr[0] as unknown) as string).toUpperCase();
   } else {
     return arr[0];
@@ -21,3 +21,60 @@ function firstElement<T>(arr: Array<T>): unknown {
 let arr = ['hello', 25, 30];
 
 console.log(firstElement(arr));
+
+interface GenericIndentityFn<Type> {
+  (arg: Type): Type;
+}
+function indentity<Type>(arg: Type): Type {
+  return arg;
+}
+let myIndentity: GenericIndentityFn<number> = indentity;
+myIndentity(5);
+
+let myNewIndentity: GenericIndentityFn<any[]> = () => [];
+myNewIndentity([]);
+
+interface LengthWise {
+  length: number;
+}
+
+function getLength<T extends LengthWise>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+getLength('');
+getLength([]);
+getLength({ length: 5, value: 5 });
+// getLength(5); //error
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+const obj = {a: 1, b: 2};
+getProperty(obj, 'a');
+// getProperty(obj, 'm') //error
+
+class BeeKeeper {
+  hasMask: boolean;
+}
+class ZooKeeper {
+  nameTag: string;
+}
+class Animal {
+  numLegs: number;
+}
+class Bee extends Animal {
+  keeper: BeeKeeper;
+}
+class Lion extends Animal {
+  keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal>(c: new () => A):A {
+  return new c();
+}
+
+let lion = createInstance(Lion);
+// lion.keeper.nameTag = 'Lion';
+lion.numLegs = 4;
+console.log(lion);
